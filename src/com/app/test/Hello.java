c3113e41a3915deee5d91ee378041e146f3ae2ce
Ishan <Ishan@Ishan-PC>
2017-11-04 14:45:43 +0530

    Initial commit

---
+++ src/com/app/test/Hello.java
@@ -0,0 +1,15 @@
+package com.app.test;  
+import javax.ws.rs.GET;  
+import javax.ws.rs.Path;  
+import javax.ws.rs.Produces;  
+import javax.ws.rs.core.MediaType;  
+@Path("/hello")  
+public class Hello {     
+  // This method is called if HTML is requested  
+  @GET  
+  @Produces(MediaType.TEXT_HTML)  
+  public String sayHtmlHello() {  
+    return "<html> " + "<title>" + "Hello Jersey" + "</title>"  
+        + "<body><h1>" + "Hello Jersey HTML" + "</h1></body>" + "</html> ";  
+  }  
+}   
