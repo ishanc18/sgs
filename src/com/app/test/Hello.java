073bc46d89e3c58a8ba993ba159f10ae5c9b6de5
Ishan <Ishan@Ishan-PC>
2017-11-04 14:59:40 +0530

    added comment in test class - Ishan

--- src/com/app/test/Hello.java
+++ src/com/app/test/Hello.java
@@ -3,6 +3,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;  
 import javax.ws.rs.Produces;  
 import javax.ws.rs.core.MediaType;  
+/**
+ * This class is for testing whether rest web service is working properly or not.
+ * Please put all the test classes in this package.
+ * @author Ishan
+ *
+ */
 @Path("/hello")  
 public class Hello {     
   // This method is called if HTML is requested  
